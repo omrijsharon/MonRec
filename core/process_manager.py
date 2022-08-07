8b3d8c2b2ff663930d84c0bba767309f4a0ca4c6
omrijsharon <omri_s@yahoo.com>
2022-08-07 15:01:57 +0300

    some order to main

--- core/process_manager.py
+++ core/process_manager.py
@@ -17,6 +17,23 @@ def stop_func(calib_readings, calib_dict, switch: str, stop_value: float):
     return calib_readings[idx] == stop_value
 
 
+def terminate_queues(queues: list):
+    try:
+        for queue in queues:
+            while not queue.empty():
+                queue.get_nowait()
+    except Exception as e:
+        print(e)
+    [queue.close() for queue in queues]
+
+
+def terminate_processes(processes: list):
+    [p.terminate() for p in processes if p.is_alive()]
+    [p.kill() for p in processes if p.is_alive()]
+    [p.join() for p in processes]
+    [p.close() for p in processes]
+
+
 def main():
     # Parameters
     resolution = {'width': 1280, 'height': 720}
@@ -43,6 +60,7 @@ def main():
     for r in range(3):
         queue_frames = Queue()
         queue_sticks = Queue()
+        stop_grab_event.clear()
         path = os.path.join(base_path, datetime.now().strftime("%Y%m%d_%H%M%S"))
         counter = 0
         quit = False
@@ -56,38 +74,24 @@ def main():
         sleep(1)
         print("Start recording #{}".format(r))
         print("path:  ", path)
-        stop_grab_event.clear()
-        # init frame processes
-        p_grab_frames = Process(target=grab_frames_to_queue, args=(queue_frames, stop_grab_event, resolution, 0))
-        p_grab_frames.daemon = True
-        p_save_frames = [Process(target=save_frames_from_queue, args=(queue_frames, path, stop_grab_event, resolution, type, compression)) for _ in range(num_workers)]
-        # init stick processes
-        p_grab_sticks = Process(target=grab_sticks_to_queue, args=(joystick, queue_sticks, stop_grab_event))
-        p_grab_sticks.daemon = True
-        p_save_sticks = Process(target=save_sticks_from_queue, args=(queue_sticks, path, calib_file, partial_stop_func, stop_grab_event, buffer_size))
 
+        # init processes
+        p_grab_frames = Process(target=grab_frames_to_queue, args=(queue_frames, stop_grab_event, resolution, 0), daemon=True)
+        p_grab_sticks = Process(target=grab_sticks_to_queue, args=(joystick, queue_sticks, stop_grab_event), daemon=True)
+        p_save_sticks = Process(target=save_sticks_from_queue, args=(queue_sticks, path, calib_file, partial_stop_func, stop_grab_event, buffer_size))
+        p_save_frames = [Process(target=save_frames_from_queue, args=(queue_frames, path, stop_grab_event, resolution, type, compression)) for _ in range(num_workers)]
         processes_list = [p_grab_frames, p_grab_sticks, p_save_sticks, *p_save_frames]
 
         # start processes
         [p.start() for p in processes_list]
+
+        # wait for processes to get a kill switch.
         while stop_grab_event.is_set() is False:
             pass
         sleep(0.5)
 
-        #stop processes and join
-        [p.terminate() for p in processes_list if p.is_alive()]
-        [p.kill() for p in processes_list if p.is_alive()]
-        [p.join() for p in processes_list]
-        [p.close() for p in processes_list]
-        try:
-            while not queue_frames.empty():
-                queue_frames.get_nowait()
-            while not queue_sticks.empty():
-                queue_frames.get_nowait()
-        except Exception as e:
-            print(e)
-        queue_frames.close()
-        queue_sticks.close()
+        terminate_processes(processes_list)
+        terminate_queues([queue_frames, queue_sticks])
         full_summary(path)
 
     # TODO(omri): Add UI
@@ -95,19 +99,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # print("start")
-    # calib_file = r"C:\Users\omrijsharon\Documents\repos\MonRec\config\frsky.json"
-    # from get_sticks import Joystick
-    # rc = Joystick()
-    # run = rc.status
-    # rc.calibrate(calib_file, load_calibration_file=True)
-    # calib_dict = json_reader(calib_file)
-    # partial_stop_func = partial(stop_func, calib_dict=calib_dict, switch="switch1", stop_value=-1.0)
-    # sleep(0.1)
-    # readings = rc.calib_read()
-    # print("stop? ", partial_stop_func(readings), readings[3])
-    # while not partial_stop_func(readings):
-    #     readings = rc.calib_read()
-    #     rc.render_axes()
-    # print("stop")
-
